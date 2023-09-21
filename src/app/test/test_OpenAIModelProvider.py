fb540d8a312cc5c2cf70ecab2f6c59847b608404
Peter Hayglass <peter.hayglass@gmail.com>
2023-09-20 19:52:26 -0500

    test openAI provider

---
+++ src/app/test/test_OpenAIModelProvider.py
@@ -0,0 +1,118 @@
+import pytest
+from pytest import MonkeyPatch
+from unittest.mock import MagicMock, AsyncMock
+import asyncio
+from typing import Any
+from discord import Message
+from IConfigManager import IConfigManager
+from ILogger import ILogger
+from ai.OpenAIModelProvider import OpenAIModelProvider
+
+
+@pytest.fixture
+def config_manager() -> IConfigManager:
+    config_manager = MagicMock(spec=IConfigManager)
+    config_manager.get_parameter.side_effect = [
+        "fake_api_key",
+        "fake_response_model",
+        "1000",
+    ]
+    return config_manager
+
+
+@pytest.fixture
+def logger() -> ILogger:
+    return MagicMock(spec=ILogger)
+
+
+@pytest.fixture
+def openai_model_provider(config_manager: IConfigManager, logger: ILogger) -> OpenAIModelProvider:
+    return OpenAIModelProvider(config_manager, logger)
+
+
+def test_init(openai_model_provider: OpenAIModelProvider) -> None:
+    assert openai_model_provider.RESPONSE_MODEL == "fake_response_model"
+    assert openai_model_provider.MAX_CONTEXT_LEN == 1000
+
+
+def test_get_response(openai_model_provider: OpenAIModelProvider, monkeypatch: MonkeyPatch) -> None:
+    msg = MagicMock(spec=Message)
+    msg.content = ""
+
+    async def mock_chat_completion(*args: Any, **kwargs: Any) -> dict:
+        completion = {"choices": [{"message": {"content": "Ai response"}}]}
+        return completion
+
+    monkeypatch.setattr("openai.ChatCompletion.acreate", mock_chat_completion)
+
+    async def run_test() -> None:
+        response = await openai_model_provider.get_response(msg)
+        assert response == "Ai response"
+    asyncio.run(run_test())
+
+
+def test_add_user_message(openai_model_provider: OpenAIModelProvider) -> None:
+    msg = MagicMock(spec=Message)
+    msg.content = "User message"
+    msg.author.display_name = "Username"
+    msg.channel.id = 1
+
+    async def run_test() -> None:
+        await openai_model_provider.add_user_message(msg)
+        assert len(openai_model_provider.history[msg.channel.id]) == 1
+        assert openai_model_provider.history[msg.channel.id][0]["content"] == "User message"
+    asyncio.run(run_test())
+
+
+def test_history_append_user(openai_model_provider: OpenAIModelProvider) -> None:
+    msg = MagicMock(spec=Message)
+    msg.content = "User message"
+    msg.author.display_name = "Username"
+    msg.channel.id = 1
+
+    async def run_test() -> None:
+        await openai_model_provider._history_append_user(msg)
+        assert len(openai_model_provider.history[msg.channel.id]) == 1
+        assert openai_model_provider.history[msg.channel.id][0]["content"] == "User message"
+    asyncio.run(run_test())
+
+
+def test_history_append_bot(openai_model_provider: OpenAIModelProvider) -> None:
+    channel_id = 1
+
+    async def run_test() -> None:
+        message = "Ai response"
+        await openai_model_provider._history_append_bot(message, channel_id)
+        assert len(openai_model_provider.history[channel_id]) == 1
+        assert openai_model_provider.history[channel_id][0]["content"] == "Ai response"
+    asyncio.run(run_test())
+
+
+def test_check_history_len(openai_model_provider: OpenAIModelProvider, monkeypatch: MonkeyPatch) -> None:
+    channel_id = 1
+
+    def mock_count_tokens(*args: Any, **kwargs: Any) -> int:
+        return 500
+
+    monkeypatch.setattr(openai_model_provider, "_count_tokens", mock_count_tokens)
+
+    async def run_test() -> None:
+        message = "Long message"
+        await openai_model_provider._history_append_bot(message, channel_id)
+        await openai_model_provider._history_append_bot(message, channel_id)
+        await openai_model_provider._check_history_len(channel_id)
+        assert len(openai_model_provider.history[channel_id]) == 1
+    
+    asyncio.run(run_test())
+
+
+def test_count_tokens(openai_model_provider: OpenAIModelProvider, monkeypatch: MonkeyPatch) -> None:
+    def mock_get_encoding(*args: Any, **kwargs: Any) -> list[int]:
+        encoding = MagicMock()
+        encoding.encode.return_value = [1, 2, 3, 4, 5]
+        return encoding
+
+    monkeypatch.setattr("tiktoken.get_encoding", mock_get_encoding)
+
+    token_count = openai_model_provider._count_tokens("test string")
+    assert token_count == 5
